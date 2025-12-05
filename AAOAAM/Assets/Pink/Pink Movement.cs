064a418923a0aa55f40d1f6df8b5e3a6949cea4e
renee <renee.harris-mitcham@mail.bcu.ac.uk>
2025-12-05 10:50:10 +0000

    Enamies
    I have added in the enemies currently "triangle as I haven't created its sprite.

--- AAOAAM/Assets/Pink/Pink Movement.cs
+++ AAOAAM/Assets/Pink/Pink Movement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PinkMovement : MonoBehaviour
 {
@@ -10,7 +11,35 @@ public class PinkMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   //PLayer moveent keys this makes it ,move with the arrow keys
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(
+                Vector2.up * 10.0f,
+                ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(
+                Vector2.right * 6.0f,
+                ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(
+                Vector2.left * 6.0f,
+                ForceMode2D.Impulse);
+
+        }
+
+        if (Camera.main.WorldToScreenPoint(transform.position).y < 0.0f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+
     }
+
+
 }
